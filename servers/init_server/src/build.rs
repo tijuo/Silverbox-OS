6c4bef1ecc0ac6d319051305f226ec2e6b6b81b1
T. Oliver <tiju.oliver@gmail.com>
2021-12-12 10:06:47 -0500

    Removed servers directory and microkernel includes

--- servers/init_server/src/build.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    println!(r"cargo:rustc-link-search=.");
-    println!(r"cargo:rustc-link-search=../../lib/libc");
-    println!(r"cargo:rustc-link-search=../../lib/libos");
-    println!(r"cargo:rustc-link-lib=c");
-    println!(r"cargo:rustc-link-lib=os_init");
-}
